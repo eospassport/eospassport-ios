05d5e4d6852740eb673299cba1e68563237bd9e2
Vitaly Berg <bergusman@gmail.com>
2018-09-23 02:41:24 +0100

    Clean

--- EOSPassport/GrantViewController.swift
+++ EOSPassport/GrantViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GrantViewController.swift
 //  EOSPassport
 //
 //  Created by Vitaliy Berg on 23/09/2018.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GrantViewController: UIViewController {
 
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
